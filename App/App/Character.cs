25ae87f1588772360213e0c2336bde510fc1a0d1
Jim South <james.a.south@gmail.com>
2020-01-07 09:50:04 -0500

    Some tests

---
+++ App/App/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public Alignment Alignment { get; set; }
+        public int ArmorClass { get; set; } = 10;
+
+        public Character() { }
+
+        public Character(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ App/Test/CharacterTests.cs
@@ -0,0 +1,124 @@
+﻿using App;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test
+{
+    public class CharacterTests
+    {
+        public class NameTests
+        {
+            [Fact]
+            public void GetsName()
+            {
+                // Arrange
+                string charName = "Grungo";
+                Character character = new Character(charName);
+
+                // Act
+                string actual = character.Name;
+
+                // Assert
+                Assert.Equal(charName, actual);
+            }
+
+            [Fact]
+            public void SetsName()
+            {
+                // Arrange
+                string charName = "Grungo";
+                Character character = new Character("");
+
+                // Act
+                character.Name = charName;
+                string actual = character.Name;
+
+                // Assert
+                Assert.Equal(charName, actual);
+            }
+        }
+
+        public class AlignmentTests
+        {
+            [Fact]
+            public void GetsAlignment()
+            {
+                // Arrange
+                Alignment alignment = Alignment.Neutral;
+                Character character = new Character();
+
+                // Act
+                Alignment actual = character.Alignment;
+
+                // Assert
+                Assert.Equal(alignment, actual);
+            }
+
+            [Fact]
+            public void SetsAlignment()
+            {
+                // Arrange
+                Alignment alignment = Alignment.Evil;
+                Character character = new Character();
+
+                // Act
+                character.Alignment = alignment;
+                Alignment actual = character.Alignment;
+
+                // Assert
+                Assert.Equal(alignment, actual);
+            }
+
+            [Theory]
+            [InlineData(Alignment.Neutral)]
+            [InlineData(Alignment.Good)]
+            [InlineData(Alignment.Evil)]
+            public void ValidValues(Alignment alignment)
+            {
+                // Arrange
+                Character character = new Character();
+
+                // Act
+                character.Alignment = alignment;
+                Alignment actual = character.Alignment;
+
+                // Assert
+                Assert.Equal(alignment, actual);
+            }
+        }
+
+        public class ArmorClassTests
+        {
+            [Fact]
+            public void DefaultIs10()
+            {
+                // Arrange
+                int armorClass = 10;
+                Character character = new Character();
+
+                // Act
+                int actual = character.ArmorClass;
+
+                // Assert
+                Assert.Equal(armorClass, actual);
+            }
+
+            //[Fact]
+            //public void SetsArmorClass()
+            //{
+            //    // Arrange
+            //    Alignment alignment = Alignment.Evil;
+            //    Character character = new Character();
+
+            //    // Act
+            //    character.Alignment = alignment;
+            //    Alignment actual = character.Alignment;
+
+            //    // Assert
+            //    Assert.Equal(alignment, actual);
+            //}
+        }
+    }
+}
