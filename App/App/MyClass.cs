204ebe4b0e296cec179321e52be9c18da8b02718
Jim South <james.a.south@gmail.com>
2020-01-07 08:34:01 -0500

    Initial commit

---
+++ App/App/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    //public class MyClass
+    //{
+    //    public static int Add(int x, int y)
+    //    {
+    //        return x + y;
+    //    }
+    //}
+}
---
+++ App/Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using App;
+using System;
+using Xunit;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+        //[Theory]
+        //[InlineData(2, 2, 4)]
+        //[InlineData(-2, 10, 8)]
+        //public void Adds(int x, int y, int expected)
+        //{
+        //    int actual = MyClass.Add(x, y);
+
+        //    Assert.Equal(expected, actual);
+        //}
+    }
+}
